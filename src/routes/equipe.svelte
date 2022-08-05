8600eed8d37975c16678520b794c8546fa85691f
AxelZublena <zubl0001@hz.nl>
2022-08-05 16:59:03 +0200

    Fix responsiveness

--- src/routes/equipe.svelte
+++ src/routes/equipe.svelte
@@ -9,7 +9,7 @@
 		eveniet quas exercitationem inventore, recusandae molestias. Quisquam expedita quis ducimus
 		debitis dicta sint reprehenderit, quaerat quibusdam natus incidunt.
 	</p>
-	<div class="w-72 mx-auto pt-10">
+	<div class="w-1/3 nav:w-72 mx-auto pt-10">
 		<img src="/img/BZFC_logo.png" alt="BZFC_logo" />
 	</div>
 </Card>
