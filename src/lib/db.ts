5f7d2d560e9c0cfd0cb1bb6cd329414d5ff25cbf
AxelZublena <zubl0001@hz.nl>
2022-08-06 23:51:30 +0200

    Fix postgresql ssh issue

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,7 @@
 import postgres from 'postgres'
 
-const DB = postgres(import.meta.env.VITE_DATABASE_URL)
+const DB = postgres(import.meta.env.VITE_DATABASE_URL, {
+	ssl: { rejectUnauthorized: false }
+})
 
 export default DB
