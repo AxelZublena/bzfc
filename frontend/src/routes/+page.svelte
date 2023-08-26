4d914be2723a2c6c7d1a4dcf10e5540dd3d2f098
AxelZublena <zubl0001@hz.nl>
2023-08-26 18:26:29 +0200

    Fresh sveltekit project

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
-	import { PortableText } from '@portabletext/svelte';
+	import Card from '../components/Card.svelte';
+	import Welcome from '../components/Welcome.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+	{#if data.posts.length}
+		{#each data.posts as post}
+			<Card {post} />
+		{/each}
+	{:else}
+		<Welcome />
+	{/if}
+</section>
